1f6a492cb1b6d273850f02857a65bd025109e8cb
Ujjawal gupta <78137129+UjjawalGupta30@users.noreply.github.com>
2024-07-30 20:50:58 +0530

    delete .next

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/ujjawalgupta/Documents/temp/src/app/layout\":[\"static/media/8b285fe5f7c506da-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
